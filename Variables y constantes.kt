2296c47810a4a5d9640d04a0d3deb320056dcd02
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2022-07-19 20:50:09 -0500

    Creando carpeta de trabajo

---
+++ Variables y constantes.kt
@@ -0,0 +1,14 @@
+/*
+* Programa de variables y constantes
+*/
+fun main() {
+    // var es para variables, es decir valores que pueden cambiar a lo largo del programa
+    var edadPerrito = 30
+    println("La edad de tu perrito es: " + edadPerrito)
+    edadPerrito = 31
+    println("La edad de tu perrito ahora es: " + edadPerrito)
+
+    // val es para constantes, es decir valores que nunca van a cambiar (read only)
+    val nombrePerrito = "Miguelín"
+    println("El nombre de tu perrito es: " + nombrePerrito)
+}
