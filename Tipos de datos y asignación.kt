b84e6eef32b741854f2eeddaa160f84b14e55c58
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2022-07-20 16:52:23 -0500

    Tipos de datos

---
+++ Tipos de datos y asignación.kt
@@ -0,0 +1,23 @@
+/*Tipos de Datos:
+ *Tipos Numericos Bit ancho
+ Double              64
+ Float               32
+ Long                64
+ Int                 32
+ Short               16
+ Byte                 8
+
+ Characters , Strings , Arrays , Booleans
+*/
+
+fun main(args: Array<String>) {
+    //Asignación implícita
+    var numero1 = 5
+    println(numero1)
+
+    //Asignación explícita
+    var numero2:Float = 18.1F
+    var numero3:Double = 18.1
+    println(numero2)
+    println(numero3)
+}
